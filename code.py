8e8924b4ac0c372a2b49fde5e1f7d96c5770e2cb
Jesse Chintanadilok <jchintanadilok@ufl.edu>
2023-04-21 18:47:59 -0400

    Working i2c with busio

--- code.py
+++ code.py
@@ -1,12 +1,45 @@
-from machine import I2C
+import gc, os
 import time
+import board, busio
 
-i2c = I2C(freq=400000)
+
+# Show available memory
+print("Memory Info - gc.mem_free()")
+print("---------------------------")
+print("{} Bytes\n".format(gc.mem_free()))
+
+flash = os.statvfs("/")
+flash_size = flash[0] * flash[2]
+flash_free = flash[0] * flash[3]
+# Show flash size
+print("Flash - os.statvfs('/')")
+print("---------------------------")
+print("Size: {} Bytes\nFree: {} Bytes\n".format(flash_size, flash_free))
+
+"""
+i2c = busio.I2C(
+    scl=board.SCL, sda=board.SDA, frequency=400000
+)  # Use board.SCL and board.SDA
+"""
+i2c = busio.I2C(
+    scl=board.SCL, sda=board.SDA, frequency=400000
+)  # Use board.SCL and board.SDA
+
+while not i2c.try_lock():
+    pass
 
 i2c.scan()
 
-while True:
-    i2c.writeto(103, "R")
-    time.sleep(100)
-    i2c.readfrom(103, 31)
-    time.sleep(100)
+try:
+    while True:
+        i2c.writeto(101, "i")
+        result = bytearray(3)
+        i2c.readfrom_into(101, result)
+        print(result)
+        i2c.writeto(101, "R")
+        result = bytearray(3)
+        i2c.readfrom_into(101, result)
+        time.sleep(1)
+
+finally:
+    i2c.unlock()
