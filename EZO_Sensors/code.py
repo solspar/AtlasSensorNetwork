61d38f3da0d801ff6a3705736554d08311818b7a
Jesse Chintanadilok <jchintanadilok@ufl.edu>
2023-04-29 18:27:29 -0400

    Support for generic ezo sensors

--- EZO_Sensors/code.py
+++ EZO_Sensors/code.py
@@ -4,7 +4,7 @@ import board, busio
 
 try:
     from AtlasSensors import *
-finally:
+except:
     print("Error importing EZO sensor module\n")
 
 # Show available memory
@@ -24,13 +24,26 @@ print("Size: {} Bytes\nFree: {} Bytes\n".format(flash_size, flash_free))
 
 # Grab i2c bus
 
+DO_ADDRESS = 97
 ORP_ADDRESS = 98
+PH_ADDRESS = 99
+EC_ADDRESS = 100
+
 
 try:
-    # identify_devices()
+    identify_devices()
     orp_sensor = generic_ezo(ORP_ADDRESS)
     res = orp_sensor.read()
-    print(res)
+    print("ORP reading:", res, "mV")
+    ph_sensor = generic_ezo(PH_ADDRESS)
+    res = ph_sensor.read()
+    print("pH:", res)
+    do_sensor = generic_ezo(DO_ADDRESS)
+    res = do_sensor.read()
+    print("Dissolved Oxygen: ", res, "mg/L")
+    ec_sensor = generic_ezo(EC_ADDRESS)
+    res = ec_sensor.read()
+    print("EC: ", res)
 
 finally:
     i2c.unlock()
