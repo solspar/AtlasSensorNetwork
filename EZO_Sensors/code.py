e8e47e8a3150c1171e48bd9f24a761751c0c01ec
Jesse Chintanadilok <jchintanadilok@ufl.edu>
2023-04-30 13:26:15 -0400

    Changed NTP server; added lib

--- EZO_Sensors/code.py
+++ EZO_Sensors/code.py
@@ -9,10 +9,7 @@ import adafruit_ntp
 import adafruit_requests as requests
 from params import headers, params
 
-try:
-    from AtlasSensors import *
-except:
-    print("Error importing EZO sensor module\n")
+from AtlasSensors import *
 
 # Get wifi details and more from a secrets.py file
 try:
@@ -33,8 +30,12 @@ print("Connected to %s!" % secrets["ssid"])
 socket = socketpool.SocketPool(wifi.radio)
 https = requests.Session(socket, ssl.create_default_context())
 
-ntp = adafruit_ntp.NTP(socket, tz_offset=0)
-rtc.RTC().datetime = ntp.datetime
+try:
+    ntp = adafruit_ntp.NTP(socket, server="time-e-g.nist.gov", tz_offset=0)
+    rtc.RTC().datetime = ntp.datetime
+except TimeoutError:
+    print("Error: Could not connect to ntp")
+    raise
 
 # Show available memory
 print("Memory Info - gc.mem_free()")
@@ -49,41 +50,41 @@ print("Flash - os.statvfs('/')")
 print("---------------------------")
 print("Size: {} Bytes\nFree: {} Bytes\n".format(flash_size, flash_free))
 
-# i2c = busio.I2C(scl=board.IO5, sda=board.IO6, frequency=400000)
-
-# Grab i2c bus
-
 DO_ADDRESS = 97
 ORP_ADDRESS = 98
 PH_ADDRESS = 99
 EC_ADDRESS = 100
 
+url = "http://159.203.186.79:8086/api/v2/write?org=uf_cea&bucket=chamber3_devel&precision=s"
+
+POLLING_PERIOD = 15  # Polling period in seconds
 
 try:
-    # identify_devices()
-    x = 0
-    while x < 15:
-        orp_sensor = generic_ezo(ORP_ADDRESS)
+    orp_sensor = generic_ezo(ORP_ADDRESS)
+    ph_sensor = generic_ezo(PH_ADDRESS)
+    do_sensor = generic_ezo(DO_ADDRESS)
+    ec_sensor = generic_ezo(EC_ADDRESS)
+    while True:
+        startTime = time.time()
         res_orp = orp_sensor.read()
         print("ORP reading:", res_orp, "mV")
 
-        ph_sensor = generic_ezo(PH_ADDRESS)
         res_ph = ph_sensor.read()
         print("pH:", res_ph)
 
-        do_sensor = generic_ezo(DO_ADDRESS)
         res_do = do_sensor.read()
         print("Dissolved Oxygen: ", res_do, "mg/L")
 
-        ec_sensor = generic_ezo(EC_ADDRESS)
         res_ec = ec_sensor.read()
         print("EC: ", res_ec)
 
-        data = '\n atlasSensors,sensor_id=310B9 orp=%f,ph=%f,do=%f,ec=%f %i' % (res_orp, res_ph,
-                                                                                res_do, res_ec,
-                                                                                time.mktime(time.localtime()))
-
-        url = "http://159.203.186.79:8086/api/v2/write?org=uf_cea&bucket=chamber3_devel&precision=s"
+        data = "\n atlasSensors,sensor_id=310B9 orp=%f,ph=%f,do=%f,ec=%f %i" % (
+            res_orp,
+            res_ph,
+            res_do,
+            res_ec,
+            time.mktime(time.localtime()),
+        )
 
         response = None
         while not response:
@@ -91,10 +92,12 @@ try:
                 response = https.post(url, headers=headers, data=data)
 
             except AssertionError as error:
-                print('Request failed')
+                print("Request failed")
 
-        x += 1
-        time.sleep(15)
+        # time.sleep(15)
+        now = time.time()
+        waitTime = POLLING_PERIOD - (now - startTime)
+        time.sleep(waitTime)
 
 finally:
     i2c.unlock()
